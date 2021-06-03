71f87b6b4d4be84bcf0aaf64e56531d6babc15f2
Sonam <sonam.dedania@gmail.com>
2021-06-03 11:12:53 +0530

    React hot toast added for bookmark

--- src/Components/city_card.jsx
+++ src/Components/city_card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactGA from 'react-ga';
 import { Row, Col, Button } from 'react-bootstrap';
+import toast, { Toaster } from 'react-hot-toast';
 
 import Sun from '../Images/sun.png';
 
@@ -44,34 +45,44 @@ class CityCard extends React.Component {
 
         let newArray = this.state.cityDetails;
         newArray[index].isBookmark = !newArray[index].isBookmark;
+        if (newArray[index].isBookmark === true) {
+            toast.success('Bookmark added successfully!!');
+        }
+        else {
+            toast.error('Bookmark removed successfully!!');
+        }
         this.setState({ cityDetails: newArray });
         this.setBookmarkCount();
     }
 
     render() {
         return (
-            this.state.cityDetails.map((cityInfo, index) => {
-                return <Col lg={4} md={6} sm={6} xs={12}>
-                    <div className="city-card">
-                        <div className="city-name" title={cityInfo.cityName}>{cityInfo.cityName}</div>
-                        <div onClick={() => this.bookmarkClicked(index)}><i className={`fa ${(cityInfo.isBookmark === true) ? 'fa-bookmark icon-bookmark' : 'fa-bookmark-o icon-bookmark-o'}`} aria-hidden="true"></i></div>
-                        <div className="tour-date">{cityInfo.tourDate}</div>
-                        <div className="category">{cityInfo.category}</div>
-                        <div className="avg-temp"><span className="border-bot">Ave</span>rage Temperature</div>
-                        <div className="city-temp">+ {cityInfo.temp}&#x00B0;C <img src={Sun} alt="" /></div>
-                        <img src={cityInfo.cityImg} className="city-img" alt="bahamas.jfif" />
-                        <Row>
-                            <Col lg={6} md={6} sm={12} xs={12}>
-                                <div className="tl-price">Total Price:</div>
-                                <div className="price">{cityInfo.price}</div>
-                            </Col>
-                            <Col lg={6} md={6} sm={12} xs={12}>
-                                <Button className="btn explore-btn">Explore</Button>
-                            </Col>
-                        </Row>
-                    </div>
-                </Col >
-            })
+            <>
+                <Toaster />
+                {this.state.cityDetails.map((cityInfo, index) => {
+                    return <Col lg={4} md={6} sm={6} xs={12}>
+                        <div className="city-card">
+                            <div className="city-name" title={cityInfo.cityName}>{cityInfo.cityName}</div>
+                            <div onClick={() => this.bookmarkClicked(index)}><i className={`fa ${(cityInfo.isBookmark === true) ? 'fa-bookmark icon-bookmark' : 'fa-bookmark-o icon-bookmark-o'}`} aria-hidden="true"></i></div>
+                            <div className="tour-date">{cityInfo.tourDate}</div>
+                            <div className="category">{cityInfo.category}</div>
+                            <div className="avg-temp"><span className="border-bot">Ave</span>rage Temperature</div>
+                            <div className="city-temp">+ {cityInfo.temp}&#x00B0;C <img src={Sun} alt="" /></div>
+                            <img src={cityInfo.cityImg} className="city-img" alt="bahamas.jfif" />
+                            <Row>
+                                <Col lg={6} md={6} sm={12} xs={12}>
+                                    <div className="tl-price">Total Price:</div>
+                                    <div className="price">{cityInfo.price}</div>
+                                </Col>
+                                <Col lg={6} md={6} sm={12} xs={12}>
+                                    <Button className="btn explore-btn">Explore</Button>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col >
+                })
+                }
+            </>
         )
     }
 }
